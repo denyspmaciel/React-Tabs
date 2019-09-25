68b6aa188da7cfeabdbd05da97a12cfb1add7fff
Denys Maciel <denysmaciel22@alu.ufc.br>
2019-09-25 11:48:39 -0300

    Update Aba2.js

--- src/components/Aba2.js
+++ src/components/Aba2.js
@@ -1,28 +1,12 @@
 import React from 'react';
 
 export default class Aba2 extends React.Component {
-    
-    state = {
-        texto: '',
-    }
-
-    atualizar = (e) => {
-        this.setState({
-            texto: e.target.value,
-        });
-    }
 
     render() {
         return (
             <div>
-                <div>
-                    <label>Digite algum texto: </label>
-                    <input type="text" name="texto" onChange={this.atualizar}/>
-                </div>
-                <div>
-                    Texto: {this.state.texto}
-                </div>
-            </div>
+                <h2>Conteúdo da aba 2</h2>
+           </div>
         );
     }
 }
